2b6457350d67e6e849b4fe3bddd611a062120810
JuanGMejia <jgmejias950513@gmail.com>
2022-07-10 12:51:00 -0500

    base interview

--- src/app/event-detail/event-details.component.ts
+++ src/app/event-detail/event-details.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {EventService} from '../services/event.service';
-import { Router } from '@angular/router';
 @Component({
   selector: 'event-details',
   templateUrl: './event-details.component.html'
